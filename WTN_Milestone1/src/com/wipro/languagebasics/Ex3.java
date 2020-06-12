418d2a62642ffe0b9b30d2ccb8e0098a2f29749e
neelu <neelu@DESKTOP-HI2B388>
2020-06-12 12:45:09 +0530

    Wipro prog Ex 3

---
+++ WTN_Milestone1/src/com/wipro/languagebasics/Ex3.java
@@ -0,0 +1,13 @@
+package com.wipro.languagebasics;
+
+public class Ex3 {
+	public static void main(String args[])
+	{
+		int a,b,s;
+		a=Integer.parseInt(args[0]);
+		b=Integer.parseInt(args[1]);
+		s=a+b;
+		System.out.println("sum of " +a+ " and " +b+" is" +s);
+	}
+
+}
