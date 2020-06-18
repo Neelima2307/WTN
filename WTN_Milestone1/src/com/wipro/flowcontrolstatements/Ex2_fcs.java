be834e9d75b422c3b46daab2d0a519e2f80c11c2
neelu <neelu@DESKTOP-HI2B388>
2020-06-18 11:53:34 +0530

    Wipro_fcs Ex 2

---
+++ WTN_Milestone1/src/com/wipro/flowcontrolstatements/Ex2_fcs.java
@@ -0,0 +1,19 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex2_fcs {
+	public static void main(String args[])
+	{
+		int num=Integer.parseInt(args[0]);
+		if(num%2==0)
+		{
+			System.out.println(num+" is a Even num");
+		}
+		else
+		{
+			System.out.println(num+" is a Odd num");
+		}
+		
+				
+	}
+
+}
