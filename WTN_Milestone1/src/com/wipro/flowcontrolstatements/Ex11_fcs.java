d9520f8a1bc1a1452f4f0ee87a46f2000719ad32
neelu <neelu@DESKTOP-HI2B388>
2020-06-22 08:13:43 +0530

    Wipro_fcs Ex 11

---
+++ WTN_Milestone1/src/com/wipro/flowcontrolstatements/Ex11_fcs.java
@@ -0,0 +1,13 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex11_fcs {
+	public static void main(String[] args)
+	{
+		for(int i=24;i<57;i=i+2)
+		{
+			System.out.print(" ");
+			System.out.print(i);
+		}
+	}
+
+}
