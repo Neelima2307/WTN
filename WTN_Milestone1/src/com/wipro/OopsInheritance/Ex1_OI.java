fdaeb7d4dbcfbf08056cd6ac10a23da7a2cba6cf
neelu <neelu@DESKTOP-HI2B388>
2020-06-24 09:36:42 +0530

    Wipro_OI Ex 1

---
+++ WTN_Milestone1/src/com/wipro/OopsInheritance/Ex1_OI.java
@@ -0,0 +1,29 @@
+package com.wipro.OopsInheritance;
+
+class Animal{
+			void eat() {
+				System.out.println("Eat");
+				
+			}
+			void sleep() {
+			System.out.println("Sleep");	
+			}
+		}
+		class Bird extends Animal{
+			void fly() {
+				System.out.println("Fly");
+			}
+		}
+public class Ex1_OI {
+ public static void main(String[] arg) {
+				Animal A=new Animal();
+				Bird B=new Bird();
+				A.eat();
+				A.sleep();
+				B.fly();
+				B.sleep();
+				B.eat();
+			}
+			
+
+}
