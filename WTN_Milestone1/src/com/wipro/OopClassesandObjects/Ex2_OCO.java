f10d65bba93d4147bec3d7201daef7c40ffa76ac
neelu <neelu@DESKTOP-HI2B388>
2020-06-24 09:29:50 +0530

    Wipro_OCO Ex 2

---
+++ WTN_Milestone1/src/com/wipro/OopClassesandObjects/Ex2_OCO.java
@@ -0,0 +1,21 @@
+package com.wipro.OopClassesandObjects;
+
+public class Ex2_OCO {
+	
+public static void main(String[] args) {
+ System.out.println(Calculator.powerDouble(13.89,4));
+ System.out.println(Calculator.powerInt(16,8));	
+		}
+	}
+class Calculator
+	{
+    static double powerInt(int num1,int num2)
+		{
+    return Math.pow(num1, num2);
+		}
+    static double powerDouble(double num1,int num2)
+		{
+    return Math.pow(num1, num2);
+		}
+	}
+
