8c09113babb146fabb1474280b62009e69518ae9
neelu <neelu@DESKTOP-HI2B388>
2020-06-24 09:24:27 +0530

    Wipro_Oda Ex 12

---
+++ WTN_Milestone1/src/com/wipro/OnedimensionalArray/Ex12_Oda.java
@@ -0,0 +1,11 @@
+package com.wipro.OnedimensionalArray;
+
+public class Ex12_Oda {
+	public static void main(String arg[])
+	{
+		int[]a=new int[] {1,2,3};
+		int[]b=new int[] {4,5,6};
+		System.out.println("{"+a[(a.length-1)/2]+","+b[(b.length-1)/2]+"}");
+	}
+
+}
