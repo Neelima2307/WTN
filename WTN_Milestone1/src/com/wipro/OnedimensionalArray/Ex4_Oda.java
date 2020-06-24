ee36cb169087e6604c84064c7443d6038b0589c9
neelu <neelu@DESKTOP-HI2B388>
2020-06-24 09:15:35 +0530

    Wipro_Oda Ex 4

---
+++ WTN_Milestone1/src/com/wipro/OnedimensionalArray/Ex4_Oda.java
@@ -0,0 +1,13 @@
+package com.wipro.OnedimensionalArray;
+
+public class Ex4_Oda {
+	public static void main(String args[])
+	{
+		char ch='a';
+		int ascii=ch;
+		int castAscii=(int) ch;
+		System.out.println("The ASCII value of"+ch+"is:"+ascii);
+		System.out.println("The ASCII value of"+ch+"is:"+castAscii);
+	}
+
+}
